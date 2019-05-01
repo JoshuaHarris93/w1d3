750e49e0f7aae3ed138ca261bacae3058cb38f0c
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-01 17:16:35 +0000

    character counting assignment

--- characterCounting.js
+++ characterCounting.js
@@ -1,3 +1,18 @@
-var hello = 'hello';
+var word = "lighthouse in the house";
+word = word.split("");
+// console.log(word);
 
-var alsdlasd;
+var obj = {};
+
+word.forEach(function(el) {
+    var check = obj.hasOwnProperty(el);
+    if (check) {
+        obj[el] += 1;
+    } else {
+        obj[el] = 1
+    }
+})
+
+delete obj[" "];
+
+console.log(obj);
